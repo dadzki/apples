65da5d8242eb3b18eaead7ce1b13d09c826666ae
Дмитрий Дацковский <dadzki@yandex.ru>
2020-09-25 20:09:22 +0400

    config

---
+++ frontend/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'M0HpH7Uzu5Q0CGML7-Nfh_EkECySw1JX',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*' ,  '192.168.56.*', '172.19.0.*']
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
